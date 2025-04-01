0eabfdbc544bc13a46df07c10c5459782292df96
erick-santos-8 <esagosto@hotmail.com>
2025-04-01 16:02:58 -0300

    titulo pagina de perfil dinamico

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,17 @@
-function ProfilePage({params}: {params: {username: string}}) {
+import { getProfileByUsername } from "@/actions/profile.action";
+
+//Mudar título da página de acordo com o nome do perfil
+export async function generateMetadata({params}:{params: {username:string}}) {
+  const user = await getProfileByUsername(params.username);
+  if(!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Visualizar o perfil de ${user.username}`,
+  }
+}
+
+async function ProfilePage({params}: {params: {username: string}}) {
   console.log(params)
   return (
     <div>page</div>
